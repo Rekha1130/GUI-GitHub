b33432abfb5e22edf8657a73becf75caafa13814
Rekha1130 <erina.rekha@gmail.com>
2023-07-16 17:43:51 -0700

    Initial Commit

---
+++ GUI-GitHub/GUI_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GUI_GitHubApp.swift
+//  GUI-GitHub
+//
+//  Created by Rekha Aldas on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GUI_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
